9639a72477d6a10cc4245af88bb9e208404513bf
Daniel Maldonado <daniel.maldonado@auravant.com>
2025-04-20 19:45:22 -0300

    Se reanuda el repositorio

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 por [Daniel Maldonado](https://danielmaldonado.com.ar) y [MUG](https://https://mug-it.org.ar)
 
 ## Ejercicios prácticos
-
-- Clase 02. Enumración de subdominios. (enumeracion_dns.py)
--- scraping01.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-req = requests.get("https://danielmaldonado.com.ar")
-soup = BeautifulSoup(req.content, 'html.parser')
-
-print(soup.find_all("h1")[0].get_text())
